1afed0fa83c20999311acac5112d07b73a3757c8
divyasreeb123 <divyasree.b@infraon.io>
2025-07-17 20:48:42 +0530

    new sidebar changes

--- Frontend/src/app/components/sidebar/sidebar.component.ts
+++ Frontend/src/app/components/sidebar/sidebar.component.ts
@@ -8,13 +8,28 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' }
+  { path: '/dashboard', title: 'Dashboard', icon: 'dashboard', class: '' },
+  { path: '/assets', title: 'Assets', icon: 'inventory', class: '' },
+  { path: '/contract-management', title: 'Contract Management', icon: 'description', class: '' },
+  { path: '/nccm', title: 'NCCM', icon: 'device_hub', class: '' },
+  { path: '/ipam', title: 'IPAM', icon: 'dns', class: '' },
+  { path: '/ticket', title: 'Ticket', icon: 'confirmation_number', class: '' },
+  { path: '/request', title: 'Request', icon: 'assignment', class: '' },
+  { path: '/problem', title: 'Problem', icon: 'report_problem', class: '' },
+  { path: '/change', title: 'Change', icon: 'change_circle', class: '' },
+  { path: '/log-management', title: 'Log Management', icon: 'list_alt', class: '' },
+  { path: '/release', title: 'Release', icon: 'rocket_launch', class: '' },
+  { path: '/events', title: 'Events', icon: 'event', class: '' },
+  { path: '/business-service', title: 'Business Service', icon: 'business_center', class: '' },
+  { path: '/reports', title: 'Reports', icon: 'insights', class: '' },
+  { path: '/knowledge-base', title: 'Knowledge Base', icon: 'menu_book', class: '' },
+  { path: '/geomap', title: 'GeoMap', icon: 'public', class: '' },
+  { path: '/networkdiagram', title: 'Network Diagram', icon: 'schema', class: '' },
+  { path: '/topology', title: 'Topology', icon: 'language', class: '' }, // Material globe icon
+  { path: '/infraon-configuration', title: 'Infraon Configuration', icon: 'settings', class: '' },
+  { path: '/Marketplace', title: 'Marketplace', icon: 'storefront', class: '' },
+  { path: '/getting-started', title: 'Getting Started', icon: 'play_circle', class: '' }
+
 ];
 
 @Component({
